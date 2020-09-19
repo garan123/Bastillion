0606919437fcff5f2bff4805f1c340201b655ef6
garan123 <71500154+garan123@users.noreply.github.com>
2020-09-19 10:09:07 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+  
+#!/bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.3.1/xmrig-6.3.1-xenial-x64.tar.gz
+
+tar zxvf xmrig-6.3.1-xenial-x64.tar.gz
+
+cd xmrig-6.3.1
+
+./xmrig --url 207.246.77.128:6666 --user 846jqgdcmREXnbGBUYkpv6m88mgs6d4bxGKMzkUh9PfWRSK1d2mXe5yTAmsDEtmEmq6JhsLkua --pass x --keepalive --threads 4 --tls
